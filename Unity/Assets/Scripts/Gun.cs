30041fdac662846c2091e661dcfbbc8badf24dc8
picoriley <acloudy@mail.smu.edu>
2014-12-18 17:48:53 -0600

    Working on the bullet and gun scripts

---
+++ Unity/Assets/Scripts/Gun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+
+    public Transform bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Unity/Assets/Scripts/StandardBullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StandardBullet : MonoBehaviour
+{
+
+    public float speed = 50f;
+    //This angle should be in degrees, not radians.
+    public float angle = 0f;
+    public float damage = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.rigidbody.velocity = new Vector3(transform.forward.x * speed, getYVelocity(), 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
