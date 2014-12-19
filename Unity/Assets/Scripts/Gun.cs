e139fa42e0ff56bce5cdc9de3ddad10bbf707b9e
picoriley <acloudy@mail.smu.edu>
2014-12-18 19:35:00 -0600

    Projectiles now fire in the correct direction!

--- Unity/Assets/Scripts/Gun.cs
+++ Unity/Assets/Scripts/Gun.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class Gun : MonoBehaviour
 {
     public Transform bullet;
-    public float spawnDistance = 3.0f;
+    public float spawnDistance = 0.5f;
+    public float timeBetweenShots = 0.5f;
+
+    private float lastShotFiredTime;
     
     // Use this for initialization
     void Start()
     {
-
+        lastShotFiredTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if ((Time.time - lastShotFiredTime) > timeBetweenShots && Input.GetMouseButton(0))
         {
-            Instantiate(bullet, transform.position + spawnDistance * transform.forward, transform.rotation);
+            Instantiate(bullet, transform.position + spawnDistance * transform.forward, this.transform.rotation);
+            lastShotFiredTime = Time.time;
         }
     }
 }
--- Unity/Assets/Scripts/StandardBullet.cs
+++ Unity/Assets/Scripts/StandardBullet.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class StandardBullet : MonoBehaviour
 {
 
-    public float speed = 50f;
+    public float speed = 1f;
     //This angle should be in degrees, not radians.
     public float angle = 0f;
     public float damage = 10f;
@@ -15,7 +15,7 @@ public class StandardBullet : MonoBehaviour
     void Start()
     {
         bullet = this.GetComponent<Rigidbody>();
-        bullet.velocity = new Vector3(transform.forward.x * speed, transform.forward.y * speed, 0.0f);
+        bullet.velocity = (bullet.transform.forward * speed);
     }
 
     // Update is called once per frame
